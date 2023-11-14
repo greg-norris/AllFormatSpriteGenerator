cb6cd82f834c210700266548f85323fe01ab1c17
greg-norris <greg.norris@ghielectronics.com>
2023-11-14 08:56:44 -0500

    Add Scaled Image

--- MakeCodeSpriteMaker/Form1.Designer.cs
+++ MakeCodeSpriteMaker/Form1.Designer.cs
@@ -35,15 +35,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(83, 62);
-            this.button1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.button1.Location = new System.Drawing.Point(42, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(269, 201);
+            this.button1.Size = new System.Drawing.Size(134, 105);
             this.button1.TabIndex = 0;
             this.button1.Text = "Load Image";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,10 +60,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(755, 58);
-            this.button2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.button2.Location = new System.Drawing.Point(378, 30);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(229, 45);
+            this.button2.Size = new System.Drawing.Size(114, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "PixelPong";
             this.button2.UseVisualStyleBackColor = true;
@@ -68,36 +72,36 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(132, 222);
-            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Location = new System.Drawing.Point(66, 115);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 3;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(83, 385);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox1.Location = new System.Drawing.Point(42, 200);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1201, 941);
+            this.textBox1.Size = new System.Drawing.Size(602, 491);
             this.textBox1.TabIndex = 4;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(403, 62);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pictureBox1.Location = new System.Drawing.Point(202, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(311, 287);
+            this.pictureBox1.Size = new System.Drawing.Size(156, 149);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(755, 111);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(378, 58);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(229, 51);
+            this.button3.Size = new System.Drawing.Size(114, 27);
             this.button3.TabIndex = 8;
             this.button3.Text = "32x8 NeoPanel";
             this.button3.UseVisualStyleBackColor = true;
@@ -105,10 +109,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(755, 179);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Location = new System.Drawing.Point(378, 93);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(229, 46);
+            this.button4.Size = new System.Drawing.Size(114, 24);
             this.button4.TabIndex = 9;
             this.button4.Text = "16x16 NeoPanel";
             this.button4.UseVisualStyleBackColor = true;
@@ -116,10 +120,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(1035, 62);
-            this.button5.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.button5.Location = new System.Drawing.Point(518, 32);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(229, 201);
+            this.button5.Size = new System.Drawing.Size(114, 105);
             this.button5.TabIndex = 10;
             this.button5.Text = "Copy to Clipboard";
             this.button5.UseVisualStyleBackColor = true;
@@ -128,18 +132,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(397, 285);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(198, 148);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 11;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(755, 242);
-            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Location = new System.Drawing.Point(378, 126);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(229, 46);
+            this.button6.Size = new System.Drawing.Size(114, 24);
             this.button6.TabIndex = 12;
             this.button6.Text = "Pulse Screen";
             this.button6.UseVisualStyleBackColor = true;
@@ -147,19 +151,66 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(755, 309);
+            this.button7.Location = new System.Drawing.Point(378, 171);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(229, 40);
+            this.button7.Size = new System.Drawing.Size(28, 21);
             this.button7.TabIndex = 13;
-            this.button7.Text = "Rave Display";
+            this.button7.Text = "1x";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(407, 171);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(28, 21);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "2x";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(436, 171);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(28, 21);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "3x";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(466, 171);
+            this.button10.Margin = new System.Windows.Forms.Padding(2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(28, 21);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "4x";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(401, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Scaled Image";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1352, 1412);
+            this.ClientSize = new System.Drawing.Size(676, 734);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.label1);
@@ -171,7 +222,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "Form1";
             this.Text = "Due Sprite Creator";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -195,6 +246,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- MakeCodeSpriteMaker/Form1.cs
+++ MakeCodeSpriteMaker/Form1.cs
@@ -249,6 +249,7 @@ namespace MakeCodeSpriteMaker {
 
         private void button6_Click(object sender, EventArgs e)
         {
+            //Pulse Screen
             Bitmap img = new Bitmap(openFileDialog1.FileName);
             var sb = new System.Text.StringBuilder();
 
@@ -310,58 +311,55 @@ namespace MakeCodeSpriteMaker {
         {
 
             Bitmap img = new Bitmap(openFileDialog1.FileName);
-
-
-            int[] index = {
-                0,31,32,63,64,95,96,127,128,159,160,191,192,223,224,255,
-                1,30,33,62,65,94,97,126,129,158,161,190,193,222,225,254,
-                2,29,34,61,66,93,98,125,130,157,162,189,194,221,226,253,
-                3,28,35,60,67,92,99,124,131,156,163,188,195,220,227,252,
-                4,27,36,59,68,91,100,123,132,155,164,187,196,219,228,251,
-                5,26,37,58,69,90,101,122,133,154,165,186,197,218,229,250,
-                6,25,38,57,70,89,102,121,134,153,166,185,198,217,230,249,
-                7,24,39,56,71,88,103,120,135,152,167,184,199,216,231,248,
-                8,23,40,55,72,87,104,119,136,151,168,183,200,215,232,247,
-                9,22,41,54,73,86,105,118,137,150,169,182,201,214,233,246,
-                10,21,42,53,74,85,106,117,138,149,170,181,202,213,234,245,
-                11,20,43,52,75,84,107,116,139,148,171,180,203,212,235,244,
-                12,19,44,51,76,83,108,115,140,147,172,179,204,211,236,243,
-                13,18,45,50,77,82,109,114,141,146,173,178,205,210,237,242,
-                14,17,46,49,78,81,110,113,142,145,174,177,206,209,238,241,
-                15,16,47,48,79,80,111,112,143,144,175,176,207,208,239,240
-
-
-             };
-
             var sb = new System.Text.StringBuilder();
 
-            sb.Append("NeoClear()");
+            sb.Append("Dim i[2+(" + img.Width + "*" + img.Height + ")] = [" + img.Width + "," + img.Height + ",");
             sb.AppendLine();
-            sb.Append("NeoShow(0,256)");
-            sb.AppendLine();
-
-            char[] myImage = new char[img.Height];
             var x = 0;
+
             for (int j = 0; j < img.Height; j++)
             {
+
                 for (int i = 0; i < img.Width; i++)
                 {
                     Color pixel = img.GetPixel(i, j);
-                    // Console.WriteLine(pixel.ToString());
-
 
                     var rValue = pixel.R.ToString();
                     var gValue = pixel.G.ToString();
                     var bValue = pixel.B.ToString();
 
-                    sb.Append("NeoSet(" + index[x] + "," + rValue + "," + gValue + "," + bValue + ")");
+                    if (rValue == "0")
+                    {
+                        sb.Append("0");
+                    }
+                    else
+                    {
+                        sb.Append("1");
+                    }
+                    if (x < img.Height * img.Width - 1)
+                    {
+                        sb.Append(",");
+                        Console.WriteLine(x);
+                    }
+                    else
+                    {
+                        sb.Append("]");
+                        Console.WriteLine(x);
+                    }
                     x++;
-                    sb.AppendLine();
                 }
                 sb.AppendLine();
+
             }
-            sb.Append("NeoShow(0,256)");
+            sb.Append("");
             sb.AppendLine();
+            sb.Append("LcdClear(0)");
+            sb.AppendLine();
+
+            sb.Append("LcdImgS(i,0,0,1,1,0)");
+
+            sb.AppendLine();
+            sb.Append("LcdShow()");
 
             textBox1.Text = sb.ToString();
             Console.WriteLine(sb);
@@ -369,8 +367,182 @@ namespace MakeCodeSpriteMaker {
 
 
 
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(openFileDialog1.FileName);
+            var sb = new System.Text.StringBuilder();
+
+            sb.Append("Dim i[2+(" + img.Width + "*" + img.Height + ")] = [" + img.Width + "," + img.Height + ",");
+            sb.AppendLine();
+            var x = 0;
+
+            for (int j = 0; j < img.Height; j++)
+            {
+
+                for (int i = 0; i < img.Width; i++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    var rValue = pixel.R.ToString();
+                    var gValue = pixel.G.ToString();
+                    var bValue = pixel.B.ToString();
+
+                    if (rValue == "0")
+                    {
+                        sb.Append("0");
+                    }
+                    else
+                    {
+                        sb.Append("1");
+                    }
+                    if (x < img.Height * img.Width - 1)
+                    {
+                        sb.Append(",");
+                        Console.WriteLine(x);
+                    }
+                    else
+                    {
+                        sb.Append("]");
+                        Console.WriteLine(x);
+                    }
+                    x++;
+                }
+                sb.AppendLine();
+
+            }
+            sb.Append("");
+            sb.AppendLine();
+            sb.Append("LcdClear(0)");
+            sb.AppendLine();
+
+            sb.Append("LcdImgS(i,0,0,2,2,0)");
+
+            sb.AppendLine();
+            sb.Append("LcdShow()");
+
+            textBox1.Text = sb.ToString();
+            Console.WriteLine(sb);
 
 
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(openFileDialog1.FileName);
+            var sb = new System.Text.StringBuilder();
+
+            sb.Append("Dim i[2+(" + img.Width + "*" + img.Height + ")] = [" + img.Width + "," + img.Height + ",");
+            sb.AppendLine();
+            var x = 0;
+
+            for (int j = 0; j < img.Height; j++)
+            {
+
+                for (int i = 0; i < img.Width; i++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    var rValue = pixel.R.ToString();
+                    var gValue = pixel.G.ToString();
+                    var bValue = pixel.B.ToString();
+
+                    if (rValue == "0")
+                    {
+                        sb.Append("0");
+                    }
+                    else
+                    {
+                        sb.Append("1");
+                    }
+                    if (x < img.Height * img.Width - 1)
+                    {
+                        sb.Append(",");
+                        Console.WriteLine(x);
+                    }
+                    else
+                    {
+                        sb.Append("]");
+                        Console.WriteLine(x);
+                    }
+                    x++;
+                }
+                sb.AppendLine();
+
+            }
+            sb.Append("");
+            sb.AppendLine();
+            sb.Append("LcdClear(0)");
+            sb.AppendLine();
+
+            sb.Append("LcdImgS(i,0,0,3,3,0)");
+
+            sb.AppendLine();
+            sb.Append("LcdShow()");
+
+            textBox1.Text = sb.ToString();
+            Console.WriteLine(sb);
+
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(openFileDialog1.FileName);
+            var sb = new System.Text.StringBuilder();
+
+            sb.Append("Dim i[2+(" + img.Width + "*" + img.Height + ")] = [" + img.Width + "," + img.Height + ",");
+            sb.AppendLine();
+            var x = 0;
+
+            for (int j = 0; j < img.Height; j++)
+            {
+
+                for (int i = 0; i < img.Width; i++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    var rValue = pixel.R.ToString();
+                    var gValue = pixel.G.ToString();
+                    var bValue = pixel.B.ToString();
+
+                    if (rValue == "0")
+                    {
+                        sb.Append("0");
+                    }
+                    else
+                    {
+                        sb.Append("1");
+                    }
+                    if (x < img.Height * img.Width - 1)
+                    {
+                        sb.Append(",");
+                        Console.WriteLine(x);
+                    }
+                    else
+                    {
+                        sb.Append("]");
+                        Console.WriteLine(x);
+                    }
+                    x++;
+                }
+                sb.AppendLine();
+
+            }
+            sb.Append("");
+            sb.AppendLine();
+            sb.Append("LcdClear(0)");
+            sb.AppendLine();
+
+            sb.Append("LcdImgS(i,0,0,4,4,0)");
+
+            sb.AppendLine();
+            sb.Append("LcdShow()");
+
+            textBox1.Text = sb.ToString();
+            Console.WriteLine(sb);
 
         }
     }
